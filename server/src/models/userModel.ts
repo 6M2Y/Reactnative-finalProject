a4d64e33aa044b08b23931f69220160d7953975b
6M2Y <minyahel.yimer@gmail.com>
2025-12-05 06:22:57 +0100

    updated final

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema<IUser>({
 
 //hash password before saving
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next(); // only hash if password is modified
+    if (!this.isModified('password')) return next(); //if password is modified
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
